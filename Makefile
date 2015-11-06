31c12c533a940cd4093b43e29eb8f019ada8fe06
Cristóbal Ocampo Quintero <cocampo5@eafit.edu.co>
2015-11-06 12:10:00 -0500

    Se modifica el Makefile, si se intentan usar instrucciones x86_64 dará error. azucar.txt tiene ahora Lorem ipsum

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
-example: example.o
-	ld -o example example.o 
-example.o: example.asm
-	nasm -f elf64 example.asm
+all: example exe 
+example: example.asm
+	nasm -f elf example.asm
+	ld -m elf_i386 -o example example.o
+	rm -f example.o 	
+exe: args.asm
+	nasm -f elf args.asm
+	ld -m elf_i386 -o exe args.o
+	rm -f args.o
 clean:
 	rm -f *.o
 	rm -f example
+	rm -f exe
--- example.asm
+++ example.asm
@@ -2,7 +2,7 @@ section .data;
 
 file db "./azucar.txt", 0
 len equ 1024
-hola_palabra: db 'Hola TARINGUEROS!!',10;
+hola_palabra: db 'Hola Mundo!',10;
 hola_tamanio: equ $-hola_palabra 
 stringu times 9 db '*'
 
@@ -26,7 +26,7 @@ mov	edx,9    ;message length
 mov	ecx,stringu   ;message to write
 mov	ebx,1    ;file descriptor (stdout)
 mov	eax,4    ;system call number (sys_write)
-int	0x80     ;call kernel
+int	0x80     ;call kernel 80h aplica
 
 mov ebx, file	;name of the file  
 mov eax, 5  
